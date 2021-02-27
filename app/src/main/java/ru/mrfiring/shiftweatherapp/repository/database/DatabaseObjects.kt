9d46211dac04bbcebd8b82cb856c116d597ec52a
Kirill Bondarev <Gut54566@yandex.ru>
2021-02-28 02:17:09 +0700

    RoomDatabase implemented Entities implemented

---
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/repository/database/DatabaseObjects.kt
@@ -0,0 +1,75 @@
+package ru.mrfiring.shiftweatherapp.repository.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class DatabaseCity(
+    @PrimaryKey(autoGenerate = false)
+    val id: Long,
+    val name: String,
+    val state: String,
+    val country: String,
+    val longitude: Double,
+    val latitude: Double
+)
+
+@Entity
+data class DatabaseWeatherContainer(
+    @PrimaryKey(autoGenerate = false)
+    val id: Long,
+    val name: String,
+    val base: String,
+    val lastUpdate: Long,
+    val timeZone: Long
+)
+
+@Entity
+data class DatabaseWeather(
+    @PrimaryKey(autoGenerate = false)
+    val id: Long,
+    val weatherId: Int,
+    val main: String,
+    val description: String,
+    val icon: String
+)
+
+@Entity
+data class DatabaseMainWeatherParameters(
+    @PrimaryKey(autoGenerate = false)
+    val id: Long,
+    val temp: Double,
+    val feelsLike: Double,
+    val tempMin: Double,
+    val tempMax: Double,
+    val pressure: Int,
+    val humidity: Double,
+    val pressureAtSeaLevel: Int,
+    val pressureAtGroundLevel: Int
+)
+
+@Entity
+data class DatabaseWind(
+    @PrimaryKey(autoGenerate = false)
+    val id:Long,
+    val speed: Double,
+    val deg: Double,
+    val gust: Double
+)
+
+@Entity
+data class DatabaseRain(
+    @PrimaryKey
+    val id: Long,
+    val forLastOneHour: Double,
+    val forLastThreeHours: Double
+)
+
+@Entity
+data class DatabaseSnow(
+    @PrimaryKey(autoGenerate = false)
+    val id: Long,
+    val forLastOneHour: Double,
+    val forLastThreeHours: Double
+)
+
---
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/repository/database/Room.kt
@@ -0,0 +1,79 @@
+package ru.mrfiring.shiftweatherapp.repository.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface CitiesDao{
+
+    @Query("select * from databasecity order by country")
+    fun getCities(): LiveData<List<DatabaseCity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCities(items:  List<DatabaseCity>)
+
+}
+
+@Dao
+interface WeatherDao{
+    @Query("select * from databaseweathercontainer where id = :id")
+    suspend fun getWeatherContainerById(id: Long): DatabaseWeatherContainer
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertWeatherContainer(item: DatabaseWeatherContainer)
+
+    @Query("select * from databaseweather where id = :id")
+    suspend fun getWeatherListById(id: Long): List<DatabaseWeather>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllWeather(items: List<DatabaseWeather>)
+
+    @Query("select * from databasemainweatherparameters where id = :id")
+    suspend fun getMainWeatherParametersById(id: Long): DatabaseMainWeatherParameters
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMainWeatherParameters(item: DatabaseMainWeatherParameters)
+
+    @Query("select * from databasewind where id = :id")
+    suspend fun getWindById(id: Long): DatabaseWind
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertWind(item: DatabaseWind)
+
+    @Query("select * from databaserain where id = :id")
+    suspend fun getRainById(id: Long): DatabaseRain
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRain(item: DatabaseRain)
+
+    @Query("select * from databasesnow where id = :id")
+    suspend fun getSnowById(id: Long): DatabaseSnow
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSnow(item: DatabaseSnow)
+}
+
+@Database(
+    entities = [DatabaseCity::class,
+        DatabaseMainWeatherParameters::class,
+        DatabaseWeatherContainer::class,
+        DatabaseWeather::class,
+        DatabaseWind::class,
+        DatabaseRain::class,
+        DatabaseSnow::class
+    ],
+    version = 1
+)
+abstract class WeatherDatabase: RoomDatabase(){
+    abstract val citiesDao: CitiesDao
+    abstract val weatherDao: WeatherDao
+}
+
+private lateinit var INSTANCE: WeatherDatabase
+fun getDatabase(context: Context): WeatherDatabase{
+    synchronized(WeatherDatabase::class.java){
+        if(!::INSTANCE.isInitialized){
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                WeatherDatabase::class.java, "weather_db"
+                )
+                .build()
+        }
+    }
+    return INSTANCE
+}
