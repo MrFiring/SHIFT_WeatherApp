fd8eff8d78b1c21ce680065659c729cac29c64ff
Kirill Bondarev <Gut54566@yandex.ru>
2021-02-28 01:14:30 +0700

    Added objects to perform the data about city weather

--- app/src/main/java/ru/mrfiring/shiftweatherapp/repository/network/NetworkObjects.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/repository/network/NetworkObjects.kt
@@ -17,3 +17,76 @@ data class Coordinates(
     @Json(name = "lon") val longitude: Double,
     @Json(name = "lat") val latitude: Double
 )
+
+@JsonClass(generateAdapter = true)
+data class WeatherContainer(
+    val coord: Coordinates,
+    val weather: Weather,
+    val base: String,
+    val main: MainWeatherParameters,
+    val wind: Wind,
+    val clouds: Clouds,
+    val rain: Rain?,
+    val snow: Snow?,
+    val dt: Long,
+    val sys: SystemInfo,
+    val timezone: Long,
+    val id: Int,
+    val name: String,
+    val cod: Int
+)
+
+
+@JsonClass(generateAdapter = true)
+data class Weather(
+    val id: Int,
+    val main: String,
+    val description: String,
+    val icon: String
+)
+
+@JsonClass(generateAdapter = true)
+data class MainWeatherParameters(
+    val temp: Double,
+    @Json(name = "feels_like") val feelsLike: Double,
+    @Json(name = "temp_min") val tempMin: Double,
+    @Json(name = "temp_max") val tempMax: Double,
+    val pressure: Int,
+    val humidity: Double,
+    @Json(name= "sea_level") val pressureAtSeaLevel: Int?,
+    @Json(name= "grnd_level") val pressureAtGroundLevel: Int?,
+)
+
+@JsonClass(generateAdapter = true)
+data class Wind(
+    val speed: Double,
+    val deg: Double,
+    val gust: Double?
+)
+
+@JsonClass(generateAdapter = true)
+data class Clouds(
+    val all: Double
+)
+
+@JsonClass(generateAdapter = true)
+data class Rain(
+    @Json(name = "1h") val forLastOneHour: Double,
+    @Json(name = "3h") val forLastThreeHours: Double
+)
+
+@JsonClass(generateAdapter = true)
+data class Snow(
+    @Json(name = "1h") val forLastOneHour: Double,
+    @Json(name = "3h") val forLastThreeHours: Double
+)
+
+@JsonClass(generateAdapter = true)
+data class SystemInfo(
+    val type: Int,
+    val id: Int,
+    val message: Double,
+    val country: String,
+    val sunrise: Long,
+    val sunset: Long
+)
