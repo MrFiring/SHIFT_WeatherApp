8748b6e509ae59a3cca9f3757fc7dc8229fbdaab
Kirill Bondarev <Gut54566@yandex.ru>
2021-02-28 18:23:17 +0700

    Added bindingAdapter to deal with status-image

--- app/src/main/java/ru/mrfiring/shiftweatherapp/BindingAdapters.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/BindingAdapters.kt
@@ -1,7 +1,10 @@
 package ru.mrfiring.shiftweatherapp
 
+import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import ru.mrfiring.shiftweatherapp.home.ApiStatus
 import ru.mrfiring.shiftweatherapp.home.HomeRecyclerViewAdapter
 import ru.mrfiring.shiftweatherapp.repository.domain.DomainCity
 
@@ -10,5 +13,22 @@ fun RecyclerView.bindData(items: List<DomainCity>?){
     items?.let {
         val adpt = adapter as HomeRecyclerViewAdapter
         adpt.submitList(items)
+    }
+}
+
+@BindingAdapter("apiStatus")
+fun ImageView.bindStatus(status: ApiStatus?){
+    when(status){
+        ApiStatus.LOADING -> {
+            visibility = View.VISIBLE
+            setImageResource(R.drawable.loading_animation)
+        }
+        ApiStatus.DONE -> {
+            visibility = View.GONE
+        }
+        ApiStatus.ERROR ->{
+            visibility = View.VISIBLE
+            setImageResource(R.drawable.ic_connection_error)
+        }
     }
 }
