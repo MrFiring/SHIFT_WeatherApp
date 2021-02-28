1b31965a9b228e1df9d5561c825723996b965a1b
Kirill Bondarev <Gut54566@yandex.ru>
2021-02-28 18:21:12 +0700

    ApiStatus added

--- app/src/main/java/ru/mrfiring/shiftweatherapp/home/HomeViewModel.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/home/HomeViewModel.kt
@@ -6,36 +6,54 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import ru.mrfiring.shiftweatherapp.repository.network.CitiesParser
-import ru.mrfiring.shiftweatherapp.repository.network.City
-import ru.mrfiring.shiftweatherapp.repository.network.OpenWeatherApi
+import ru.mrfiring.shiftweatherapp.repository.WeatherRepository
+import ru.mrfiring.shiftweatherapp.repository.domain.DomainCity
+import ru.mrfiring.shiftweatherapp.repository.domain.asDomainObject
+import ru.mrfiring.shiftweatherapp.repository.getRepository
+import ru.mrfiring.shiftweatherapp.repository.network.*
 import java.lang.Exception
 
+enum class ApiStatus { LOADING, ERROR, DONE}
+
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
-    private val _response = MutableLiveData<List<City>>()
-    val response: LiveData<List<City>>
-    get() = _response
+
+    private val _status = MutableLiveData<ApiStatus>()
+    val status: LiveData<ApiStatus>
+    get() = _status
+
+    private val _navigateToDetails = MutableLiveData<DomainCity?>()
+    val navigateToDetails: LiveData<DomainCity?>
+    get() = _navigateToDetails
+
+    private val repository = getRepository(application.applicationContext)
+    val cities = repository.cities
 
 
     init{
         refreshDataFromServer()
     }
 
+
     private fun refreshDataFromServer() = viewModelScope.launch {
         try{
-            val responseBody = OpenWeatherApi.openWeatherService.getCitiesFile()
-            val parser = CitiesParser()
-            val rawData = parser.decompressGZip(responseBody)
-            val list = parser.parseJson(rawData)
-
-            _response.value = list
-
+            _status.value = ApiStatus.LOADING
+            if(cities.value.isNullOrEmpty()) {
+                repository.loadCitiesFromServer()
+                _status.value = ApiStatus.DONE
+            }
         }catch (ex: Exception){
+            _status.value = ApiStatus.ERROR
             Log.e("HomeViewModel", ex.toString())
         }
+    }
+
+    fun onCityClicked(city: DomainCity) {
+        _navigateToDetails.value = city
+    }
+
+    fun doneNavigating(){
+        _navigateToDetails.value = null
     }
 
 }
