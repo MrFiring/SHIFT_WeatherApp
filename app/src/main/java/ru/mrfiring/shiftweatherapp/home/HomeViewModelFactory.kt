e96026f18209898a418012e87c6bfac18ad6dd86
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-02 12:04:26 +0700

    added Annotation

--- app/src/main/java/ru/mrfiring/shiftweatherapp/home/HomeViewModelFactory.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/home/HomeViewModelFactory.kt
@@ -3,10 +3,12 @@ package ru.mrfiring.shiftweatherapp.home
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.paging.ExperimentalPagingApi
 import java.lang.IllegalArgumentException
 
 @Suppress("UNCHECKED_CAST")
 class HomeViewModelFactory(private val application: Application): ViewModelProvider.Factory {
+    @ExperimentalPagingApi
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(HomeViewModel::class.java)){
             return HomeViewModel(application) as T
