3305b3f1d33a0dd40e39a0ea9e64501dda932b5c
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-04 23:40:02 +0700

    Full implementation of Compose Navigation

--- app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/MainActivity.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/MainActivity.kt
@@ -1,12 +1,49 @@
 package ru.mrfiring.shiftweatherapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.mrfiring.shiftweatherapp.R
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import androidx.paging.ExperimentalPagingApi
+import ru.mrfiring.shiftweatherapp.presentation.detail.DetailScreen
+import ru.mrfiring.shiftweatherapp.presentation.home.HomeScreen
+
+
+object Navigations {
+    const val Home = "home"
+    const val Details = "details"
+
+}
 
 class MainActivity : AppCompatActivity() {
+    @ExperimentalPagingApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        setContent {
+            val navController = rememberNavController()
+            MaterialTheme {
+                NavHost(navController = navController, startDestination = Navigations.Home) {
+                    composable(route = Navigations.Home) {
+                        HomeScreen(navController = navController)
+                    }
+                    composable(
+                        route = "${Navigations.Details}/{cityId}",
+                        arguments = listOf(navArgument("cityId") { type = NavType.LongType })
+                    ) { backStackEntry ->
+                        backStackEntry.arguments?.getLong("cityId")?.let {
+                            DetailScreen(navController = navController, it)
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
