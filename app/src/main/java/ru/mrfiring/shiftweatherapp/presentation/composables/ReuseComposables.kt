af93c692cba4139a4eddaf117c19f2bd1ed46dc5
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-05 00:09:26 +0700

    Implemented ShowAppBar

--- app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/composables/ReuseComposables.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/composables/ReuseComposables.kt
@@ -2,17 +2,28 @@ package ru.mrfiring.shiftweatherapp.presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import ru.mrfiring.shiftweatherapp.R
+
+@Composable
+fun ShowAppBar(title: String) {
+    val primaryCol = colorResource(id = R.color.colorPrimary)
+    val contentColor = colorResource(id = R.color.white)
+    TopAppBar(
+        title = {
+            Text(text = title, style = MaterialTheme.typography.h5)
+        },
+        backgroundColor = primaryCol,
+        contentColor = contentColor
+    )
+}
 
 @Composable
 fun ShowLoading() {
