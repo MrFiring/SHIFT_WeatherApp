29f7c8a302d041e1e4999cb5d11ec6e1f5bd1da9
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-06 14:44:05 +0700

    Removed unused things

--- app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/home/HomeViewModel.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/home/HomeViewModel.kt
@@ -2,32 +2,18 @@ package ru.mrfiring.shiftweatherapp.presentation.home
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import androidx.paging.ExperimentalPagingApi
-import androidx.paging.LoadState
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import kotlinx.coroutines.flow.Flow
 import ru.mrfiring.shiftweatherapp.domain.DomainCity
 import ru.mrfiring.shiftweatherapp.domain.GetCitiesLiveDataUseCase
-import ru.mrfiring.shiftweatherapp.presentation.SingleLiveEvent
-import ru.mrfiring.shiftweatherapp.presentation.detail.ApiStatus
-
 
 @ExperimentalPagingApi
 class HomeViewModel(application: Application,
                     private val getCitiesFlowUseCase: GetCitiesLiveDataUseCase
 ) : AndroidViewModel(application) {
-
-    private val _status = MutableLiveData<ApiStatus>()
-    val status: LiveData<ApiStatus>
-        get() = _status
-
-    private val _navigateToDetails = SingleLiveEvent<DomainCity?>()
-    val navigateToDetails: LiveData<DomainCity?>
-        get() = _navigateToDetails
 
     private lateinit var _cities: Flow<PagingData<DomainCity>>
     val cities: Flow<PagingData<DomainCity>>
@@ -37,27 +23,9 @@ class HomeViewModel(application: Application,
         bindData()
     }
 
-
     @ExperimentalPagingApi
     private fun bindData() {
         _cities = getCitiesFlowUseCase().cachedIn(viewModelScope)
     }
-
-    fun onCityClicked(city: DomainCity) {
-        _navigateToDetails.value = city
-    }
-
-    fun onLoadStateEvent(refreshLoadState: LoadState) {
-        _status.value = if(refreshLoadState is LoadState.Loading && _status.value != ApiStatus.LOADING){
-            ApiStatus.LOADING
-        }else if(refreshLoadState is LoadState.Error && _status.value != ApiStatus.ERROR){
-            ApiStatus.ERROR
-        }else{
-            ApiStatus.DONE
-        }
-
-    }
-
-
 
 }
