5c387e2d37472b56996e8f2a5ad4e55fcee01adc
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-06 23:23:00 +0700

    Added route to splash and WeatherTheme

--- app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/MainActivity.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/MainActivity.kt
@@ -1,9 +1,9 @@
 package ru.mrfiring.shiftweatherapp.presentation
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -12,15 +12,18 @@ import androidx.navigation.compose.rememberNavController
 import androidx.paging.ExperimentalPagingApi
 import ru.mrfiring.shiftweatherapp.presentation.detail.DetailScreen
 import ru.mrfiring.shiftweatherapp.presentation.home.HomeScreen
+import ru.mrfiring.shiftweatherapp.presentation.splash.SplashScreen
+import ru.mrfiring.shiftweatherapp.presentation.theme.WeatherTheme
 
 
 object Navigations {
+    const val Splash = "splash"
     const val Home = "home"
     const val Details = "details"
 
 }
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     @ExperimentalPagingApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,20 +31,26 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
             val navController = rememberNavController()
-            MaterialTheme {
-                NavHost(navController = navController, startDestination = Navigations.Home) {
-                    composable(route = Navigations.Home) {
-                        HomeScreen(navController = navController)
-                    }
-                    composable(
-                        route = "${Navigations.Details}/{cityId}",
-                        arguments = listOf(navArgument("cityId") { type = NavType.LongType })
-                    ) { backStackEntry ->
-                        backStackEntry.arguments?.getLong("cityId")?.let {
-                            DetailScreen(navController = navController, it)
+            WeatherTheme(darkTheme = true) {
+                Surface {
+                    NavHost(navController = navController, startDestination = Navigations.Splash) {
+                        composable(route = Navigations.Splash) {
+                            SplashScreen(navController = navController)
+                        }
+                        composable(route = Navigations.Home) {
+                            HomeScreen(navController = navController)
+                        }
+                        composable(
+                            route = "${Navigations.Details}/{cityId}",
+                            arguments = listOf(navArgument("cityId") { type = NavType.LongType })
+                        ) { backStackEntry ->
+                            backStackEntry.arguments?.getLong("cityId")?.let {
+                                DetailScreen(navController = navController, it)
+                            }
                         }
                     }
                 }
+
             }
         }
 
