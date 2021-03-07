cc0a659131612fb5da7e2ca4bdb839920ddfc640
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-08 00:13:09 +0700

    AppBar and Drawer removed

--- app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/home/HomeScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
@@ -17,68 +16,58 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
-import ru.mrfiring.shiftweatherapp.di.get
 import ru.mrfiring.shiftweatherapp.di.getViewModel
 import ru.mrfiring.shiftweatherapp.presentation.Navigations
-import ru.mrfiring.shiftweatherapp.presentation.composables.ShowAppBar
 import ru.mrfiring.shiftweatherapp.presentation.composables.ShowLoading
 import ru.mrfiring.shiftweatherapp.presentation.composables.ShowNetworkError
 import ru.mrfiring.shiftweatherapp.presentation.composables.ThemeAwareCard
 import ru.mrfiring.shiftweatherapp.presentation.composables.home.CityItem
-import ru.mrfiring.shiftweatherapp.presentation.composables.home.ShowHomeDrawer
-import ru.mrfiring.shiftweatherapp.presentation.composables.home.items
 
 @ExperimentalPagingApi
 @Composable
 fun HomeScreen(
     navController: NavController,
-    themeState: MutableState<Boolean> = get(),
     viewModel: HomeViewModel = getViewModel()
 ) {
     val lazyPagingItems = viewModel.cities.collectAsLazyPagingItems()
     val lazyListState = rememberLazyListState()
 
     Surface(modifier = Modifier.fillMaxSize()) {
-        ShowHomeDrawer(onBtnClick = { themeState.value = !themeState.value }, items = items) {
-            Column {
-                ShowAppBar(
-                    title = "Weather"
-                )
-                LazyColumn(state = lazyListState) {
-                    items(lazyPagingItems) {
-                        it?.let { city ->
-                            CityItem(domainCity = city) {
-                                navController.navigate("${Navigations.Details}/${city.id}") {
-                                    //Need to navigateUp correctly on a real device
-                                    popUpTo(Navigations.Home) { inclusive = false }
-                                }
+        Column {
+            LazyColumn(state = lazyListState) {
+                items(lazyPagingItems) {
+                    it?.let { city ->
+                        CityItem(domainCity = city) {
+                            navController.navigate("${Navigations.Details}/${city.id}") {
+                                //Need to navigateUp correctly on a real device
+                                popUpTo(Navigations.Home) { inclusive = false }
                             }
                         }
                     }
+                }
 
-                    //Show loading state by appending item to the LazyColumn
-                    lazyPagingItems.apply {
-                        //Only check refresh state. Others are minor.
-                        when (loadState.refresh) {
-                            is LoadState.Loading -> {
-                                item {
-                                    ThemeAwareCard(modifier = Modifier.fillMaxWidth()) {
-                                        ShowLoading(modifier = Modifier.fillMaxSize())
-                                    }
-                                }
-
-                            }
-                            is LoadState.Error -> {
-                                item {
-                                    ShowNetworkError(
-                                        modifier = Modifier.fillMaxSize(),
-                                        onRetry = lazyPagingItems::retry
-                                    )
+                //Show loading state by appending item to the LazyColumn
+                lazyPagingItems.apply {
+                    //Only check refresh state. Others are minor.
+                    when (loadState.refresh) {
+                        is LoadState.Loading -> {
+                            item {
+                                ThemeAwareCard(modifier = Modifier.fillMaxWidth()) {
+                                    ShowLoading(modifier = Modifier.fillMaxSize())
                                 }
                             }
-                            //Do nothing
-                            else -> {
+
+                        }
+                        is LoadState.Error -> {
+                            item {
+                                ShowNetworkError(
+                                    modifier = Modifier.fillMaxSize(),
+                                    onRetry = lazyPagingItems::retry
+                                )
                             }
+                        }
+                        //Do nothing
+                        else -> {
                         }
                     }
                 }
