4a4281f4eede6cbb821033de5f20c2c94ec49070
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-08 02:04:43 +0700

    FavoriteScreen first prototype ready. Added retry method

--- app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/favorite/FavoriteScreen.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/favorite/FavoriteScreen.kt
@@ -1,8 +1,19 @@
 package ru.mrfiring.shiftweatherapp.presentation.favorite
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import ru.mrfiring.shiftweatherapp.di.getViewModel
+import ru.mrfiring.shiftweatherapp.presentation.composables.ShowLoading
+import ru.mrfiring.shiftweatherapp.presentation.composables.ShowNetworkError
+import ru.mrfiring.shiftweatherapp.presentation.composables.home.CityItem
+import ru.mrfiring.shiftweatherapp.presentation.detail.ApiStatus
 
 
 @Composable
@@ -10,7 +21,39 @@ fun FavoriteScreen(
     navController: NavController,
     viewModel: FavoriteScreenViewModel = getViewModel()
 ) {
+    val favorites by viewModel.favorites.observeAsState(initial = emptyList())
+    val status by viewModel.status.observeAsState(initial = ApiStatus.LOADING)
 
-    //Place components)
+    Column(modifier = Modifier.fillMaxWidth()) {
 
+        LazyColumn() {
+
+            items(favorites) {
+                CityItem(domainCity = it, onClick = {
+                    //Do nothing)
+                })
+            }
+
+            favorites.apply {
+                when (status) {
+                    ApiStatus.LOADING -> {
+                        item {
+                            ShowLoading()
+                        }
+                    }
+                    ApiStatus.ERROR -> {
+                        item {
+                            ShowNetworkError(onRetry = viewModel::onRetry)
+                        }
+                    }
+                    else -> {
+                        //Do nothing)
+                    }
+
+                }
+            }
+        }
+
+
+    }
 }
--- app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/favorite/FavoriteScreenViewModel.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/favorite/FavoriteScreenViewModel.kt
@@ -41,4 +41,6 @@ class FavoriteScreenViewModel(
 
     }
 
+    fun onRetry() = bindFavorites()
+
 }
