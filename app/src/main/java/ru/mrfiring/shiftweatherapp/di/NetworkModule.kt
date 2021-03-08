264418a1e7b07ba7eeb682336b637aefbce10b51
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-09 02:21:26 +0700

    Added callAdapterFactory for retrofit

--- app/src/main/java/ru/mrfiring/shiftweatherapp/di/NetworkModule.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/di/NetworkModule.kt
@@ -4,6 +4,7 @@ import androidx.paging.ExperimentalPagingApi
 import com.squareup.moshi.Moshi
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import ru.mrfiring.shiftweatherapp.data.CitiesParser
 import ru.mrfiring.shiftweatherapp.data.CitiesParserImpl
@@ -11,13 +12,13 @@ import ru.mrfiring.shiftweatherapp.data.database.CitiesDao
 import ru.mrfiring.shiftweatherapp.data.network.BASE_URL
 import ru.mrfiring.shiftweatherapp.data.network.OpenWeatherService
 import ru.mrfiring.shiftweatherapp.data.paging.CityMediator
-import ru.mrfiring.shiftweatherapp.domain.CitiesRepository
 
 @ExperimentalPagingApi
 val networkModule = module {
     fun provideRetrofit(baseUrl: String): Retrofit {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
