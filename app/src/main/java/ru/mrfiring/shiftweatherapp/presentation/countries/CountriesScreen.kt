4afe1413a837a7c4943859fae4ddcac06f105e85
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-08 15:45:05 +0700

    Created a template for CountriesScreen with VM

---
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/countries/CountriesScreen.kt
@@ -0,0 +1,13 @@
+package ru.mrfiring.shiftweatherapp.presentation.countries
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import ru.mrfiring.shiftweatherapp.di.getViewModel
+
+@Composable
+fun CountriesScreen(
+    navController: NavController,
+    viewModel: CountriesScreenViewModel = getViewModel()
+) {
+
+}
---
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/countries/CountriesScreenViewModel.kt
@@ -0,0 +1,11 @@
+package ru.mrfiring.shiftweatherapp.presentation.countries
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class CountriesScreenViewModel(
+    application: Application
+
+) : AndroidViewModel(application) {
+
+}
