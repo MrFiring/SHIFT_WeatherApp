6fe3666162fe09cf58832c7d5176fb7147b6d585
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-09 16:29:28 +0700

    upd

--- app/src/main/java/ru/mrfiring/shiftweatherapp/data/database/Room.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/data/database/Room.kt
@@ -2,7 +2,6 @@ package ru.mrfiring.shiftweatherapp.data.database
 
 import androidx.paging.PagingSource
 import androidx.room.*
-import io.reactivex.Completable
 import io.reactivex.Maybe
 import io.reactivex.Single
 
@@ -16,7 +15,7 @@ interface CitiesDao{
     fun getCityById(id: Long): Single<DatabaseCity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertCities(items: List<DatabaseCity>): Completable
+    fun insertCities(items: List<DatabaseCity>)
 
     @Query("select count(id) from databasecity")
     fun getCountOfCities(): Int
@@ -28,37 +27,37 @@ interface WeatherDao{
     fun getWeatherContainerById(id: Long): Maybe<DatabaseWeatherContainer>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertWeatherContainer(item: DatabaseWeatherContainer): Completable
+    fun insertWeatherContainer(item: DatabaseWeatherContainer)
 
     @Query("select * from databaseweather where id = :id")
     fun getWeatherListById(id: Long): Maybe<List<DatabaseWeather>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAllWeather(items: List<DatabaseWeather>): Completable
+    fun insertAllWeather(items: List<DatabaseWeather>)
 
     @Query("select * from databasemainweatherparameters where id = :id")
     fun getMainWeatherParametersById(id: Long): Maybe<DatabaseMainWeatherParameters>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertMainWeatherParameters(item: DatabaseMainWeatherParameters): Completable
+    fun insertMainWeatherParameters(item: DatabaseMainWeatherParameters)
 
     @Query("select * from databasewind where id = :id")
     fun getWindById(id: Long): Maybe<DatabaseWind>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertWind(item: DatabaseWind): Completable
+    fun insertWind(item: DatabaseWind)
 
     @Query("select * from databaserain where id = :id")
     fun getRainById(id: Long): Maybe<DatabaseRain>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertRain(item: DatabaseRain): Completable
+    fun insertRain(item: DatabaseRain)
 
     @Query("select * from databasesnow where id = :id")
     fun getSnowById(id: Long): Maybe<DatabaseSnow>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertSnow(item: DatabaseSnow): Completable
+    fun insertSnow(item: DatabaseSnow)
 }
 
 
