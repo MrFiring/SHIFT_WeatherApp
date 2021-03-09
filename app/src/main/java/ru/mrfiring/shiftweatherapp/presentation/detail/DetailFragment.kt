10c71b8197e6e0a4184a87808f1b9f169b79cc31
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-09 14:47:08 +0700

    Bugfix with SharedViewModel

--- app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/detail/DetailFragment.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/detail/DetailFragment.kt
@@ -1,11 +1,11 @@
 package ru.mrfiring.shiftweatherapp.presentation.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import androidx.fragment.app.Fragment
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 import org.koin.core.parameter.parametersOf
 import ru.mrfiring.shiftweatherapp.databinding.DetailFragmentBinding
 
@@ -17,9 +17,8 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = DetailFragmentBinding.inflate(inflater)
-        val application = requireNotNull(activity).application
         val argument = DetailFragmentArgs.fromBundle(requireArguments())
-        val viewModel: DetailViewModel by sharedViewModel { parametersOf(argument.cityId)}
+        val viewModel: DetailViewModel = getViewModel { parametersOf(argument.cityId) }
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
