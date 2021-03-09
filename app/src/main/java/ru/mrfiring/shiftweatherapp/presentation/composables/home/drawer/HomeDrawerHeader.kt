f2b6aac8b920cc2c21785c5fd577941323045a8f
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-09 21:26:05 +0700

    Image changed to IconButton, it fixed bug related to disappear of image due to theme change

--- app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/composables/home/drawer/HomeDrawerHeader.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/composables/home/drawer/HomeDrawerHeader.kt
@@ -2,15 +2,18 @@ package ru.mrfiring.shiftweatherapp.presentation.composables.home.drawer
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,8 +31,8 @@ fun HomeDrawerHeader(onClick: () -> Unit) {
             .padding(horizontal = 8.dp)
     ) {
         val switcher = painterResource(id = R.drawable.ic_theme_switcher)
-        Box(
-            contentAlignment = Alignment.Center,
+        IconButton(
+            onClick = onClick,
             modifier = Modifier
                 .background(
                     color = if (isLight) {
@@ -42,14 +45,13 @@ fun HomeDrawerHeader(onClick: () -> Unit) {
                 .padding(8.dp)
                 .size(32.dp, 30.dp)
         ) {
-            Image(
+            Icon(
                 painter = switcher,
                 contentDescription = "Switch theme",
-                modifier = Modifier.clickable { onClick() },
-                colorFilter = if (isLight) {
-                    ColorFilter.tint(MaterialTheme.colors.primary)
+                tint = if (isLight) {
+                    MaterialTheme.colors.primary
                 } else {
-                    ColorFilter.tint(MaterialTheme.colors.onPrimary)
+                    MaterialTheme.colors.onPrimary
                 }
             )
         }
