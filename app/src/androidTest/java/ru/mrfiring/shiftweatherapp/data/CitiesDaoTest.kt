a6b2590cc17e78d6093ea55d5a74d26565682e64
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-11 23:28:21 +0700

    Added some tests for citiesDao

--- app/src/androidTest/java/ru/mrfiring/shiftweatherapp/data/CitiesDaoTest.kt
+++ app/src/androidTest/java/ru/mrfiring/shiftweatherapp/data/CitiesDaoTest.kt
@@ -4,18 +4,21 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import ru.mrfiring.shiftweatherapp.data.database.CitiesDao
 import ru.mrfiring.shiftweatherapp.data.database.DatabaseCity
 import ru.mrfiring.shiftweatherapp.data.database.WeatherDatabase
+import ru.mrfiring.shiftweatherapp.getOrAwaitValue
 import java.io.IOException
 
 @ExperimentalCoroutinesApi
-class citiesDaoTest {
+class CitiesDaoTest {
     private lateinit var citiesDao: CitiesDao
     private lateinit var database: WeatherDatabase
 
@@ -42,6 +45,43 @@ class citiesDaoTest {
         database.close()
     }
 
+    @Test
+    @Throws(IOException::class)
+    fun insertCitiesAndGetCountOfThem() = runBlocking {
+        citiesDao.insertCities(cities)
+        val count = citiesDao.getCountOfCities()
+        assertEquals(cities.size, count)
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun insertSomeCityUpdateItAndCheck() = runBlocking {
+        val itemToReplace = cities[0].copy(favorite = true)
+        citiesDao.insertCities(cities)
+
+        citiesDao.updateCity(itemToReplace)
+
+        val result = citiesDao.getCityById(1)
+
+        assertEquals(itemToReplace.favorite, result.favorite)
+
+    }
+
+    //doesn't work
+    @Test
+    @Throws(IOException::class)
+    fun insertCitiesAndGetFavoriteLiveData() = runBlocking {
+        citiesDao.insertCities(cities)
+
+        withContext(Dispatchers.Main) {
+            val result = citiesDao.getFavoriteCities().getOrAwaitValue()
+
+            assertEquals(
+                cities.filter { it.favorite }.size,
+                result.size
+            )
+        }
+    }
 
     @Test
     @Throws(IOException::class)
