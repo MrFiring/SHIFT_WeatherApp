d5874fcac928ca699c9101cc3b09c3f661d8857a
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-11 17:15:24 +0700

    Implemented CitiesViewModelTest.kt

--- app/src/test/java/ru/mrfiring/shiftweatherapp/presentation/CitiesViewModelTest.kt
+++ app/src/test/java/ru/mrfiring/shiftweatherapp/presentation/CitiesViewModelTest.kt
@@ -1,5 +1,69 @@
 package ru.mrfiring.shiftweatherapp.presentation
 
+import android.app.Application
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.PagingData
+import io.mockk.*
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import ru.mrfiring.shiftweatherapp.CoroutineTestRule
+import ru.mrfiring.shiftweatherapp.domain.CitiesRepository
+import ru.mrfiring.shiftweatherapp.domain.GetCitiesFlowUseCase
+import ru.mrfiring.shiftweatherapp.domain.UpdateCityUseCase
+import ru.mrfiring.shiftweatherapp.domain.models.DomainCity
+import ru.mrfiring.shiftweatherapp.presentation.cities.CitiesScreenViewModel
+
+@ExperimentalCoroutinesApi
 class CitiesViewModelTest {
+    private val application: Application = mockk()
+
+    private val citiesRepository: CitiesRepository = mockk()
+
+    private val getCitiesFlowUseCase: GetCitiesFlowUseCase = mockk()
+    private val updateCityUseCase: UpdateCityUseCase = UpdateCityUseCase(citiesRepository)
+    private val pagingFlow: Flow<PagingData<DomainCity>> = mockk()
+
+
+    @get:Rule
+    val coroutineRule = CoroutineTestRule()
+
+    private val domainCity: DomainCity = DomainCity(
+        15,
+        "",
+        "",
+        "",
+        15.0,
+        40.0,
+        false
+    )
+
+    @Before
+    fun provideFlow() {
+
+    }
+
+    @ExperimentalPagingApi
+    @Test
+    fun `update city EXPECTED to call useCase update city and invert favorite`() = runBlockingTest {
+        coEvery { getCitiesFlowUseCase() } returns pagingFlow
+        //There is no way to move VM into class body due to init block
+        @ExperimentalPagingApi
+        val citiesScreenViewModel = CitiesScreenViewModel(
+            application,
+            getCitiesFlowUseCase,
+            updateCityUseCase
+        )
+        coEvery { updateCityUseCase(domainCity) } just runs
+
+        citiesScreenViewModel.onLongPress(domainCity)
+
+        coVerify { updateCityUseCase(domainCity) }
+        assertEquals(true, domainCity.favorite)
+    }
 
 }
