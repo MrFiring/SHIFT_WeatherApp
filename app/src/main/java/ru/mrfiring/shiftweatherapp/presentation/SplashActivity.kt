a731453d5c90a10c9fc87662f28a3b97734b171e
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-17 20:14:57 +0700

    Migration to mavenCentral Compose nav upd Gradle upd

--- app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/SplashActivity.kt
+++ app/src/main/java/ru/mrfiring/shiftweatherapp/presentation/SplashActivity.kt
@@ -3,8 +3,10 @@ package ru.mrfiring.shiftweatherapp.presentation
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.ExperimentalFoundationApi
 
 class SplashActivity : AppCompatActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
